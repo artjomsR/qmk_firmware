4275c608aad1240814f779beda9c46edca6a0284
Art Riz <artjoms.rizihs@gmail.com>
2024-03-24 22:25:53 +0000

    Initial

---
+++ users/art/config.h
@@ -0,0 +1,17 @@
+// Copyright 2022 Artjoms Rizihs (@artjomsR)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+
+#define COMBO_ONLY_FROM_LAYER 0
+
+#define COMBO_SHOULD_TRIGGER
+#define COMBO_TERM 32
+
+// saving space
+#define LAYER_STATE_16BIT // remove if using more than 16 layers
+#define NO_ACTION_ONESHOT
+#define NO_MUSIC_MODE
